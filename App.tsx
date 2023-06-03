38b2adf704b1727b2937fc1dc52d9d3fbfd556ab
aaronfg <afg@aaronfg.com>
2023-06-03 01:39:17 -0600

    more stack nav incremental work

--- App.tsx
+++ App.tsx
@@ -7,15 +7,10 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { LoginStack } from './src/navigation/LoginStack';
+import { LoginScreen } from './src/screens/Login';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -23,16 +18,10 @@ function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <SafeAreaView style={{ backgroundColor: '#FFFFFF' }}>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={{ backgroundColor: '#FFFFFF' }}>
-            <View>
-              <Text>Blah</Text>
-            </View>
-          </ScrollView>
-        </SafeAreaView>
+        <StatusBar barStyle="dark-content" backgroundColor={'#FFFFFF'} />
+        <LoginStack.Navigator>
+          <LoginStack.Screen name="Login" component={LoginScreen} />
+        </LoginStack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const LoginScreen = (): JSX.Element => {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Login Screen!</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
