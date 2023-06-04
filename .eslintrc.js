0c63c940cc0e33e2f0d682be712affb104ac044b
aaronfg <afg@aaronfg.com>
2023-06-03 18:55:04 -0600

    mess of changes to get jest fixed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: ['prettier'],
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native-community',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
 };
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+/** The Login screen for adding an account to the app. */
 export const LoginScreen = (): JSX.Element => {
   return (
     <SafeAreaView>
