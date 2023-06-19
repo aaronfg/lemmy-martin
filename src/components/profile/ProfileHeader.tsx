cdd3defc1ad803a8d320840e506a8172d70981cf
aaronfg <afg@aaronfg.com>
2023-06-18 23:40:02 -0600

    fixed header not collapsing on item click

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { MD3Theme, useTheme } from 'react-native-paper';
 import { getSettingsCurrentAccount } from '../../features/settings/selectors';
 import { useAppSelector } from '../../redux/hooks';
-import { ExpandableView } from '../ExpandableView';
+import { ExpandableView, ExpandableViewType } from '../ExpandableView';
 import { AccountSwitcher } from './AccountSwitcher';
 import { ProfileChooser } from './ProfileChooser';
 
@@ -12,11 +12,18 @@ export const ProfileHeader = (): JSX.Element => {
   const theme = useTheme();
   const styles = createStyleSheet(theme);
 
+  const exp = useRef<ExpandableViewType>(null);
+
+  const onAccountItemClick = () => {
+    exp.current?.close();
+  };
+
   return (
     <View style={styles.container}>
       <ExpandableView
+        ref={exp}
         headerView={<AccountSwitcher currentAccount={currentAccount} />}
-        contentView={<ProfileChooser />}
+        contentView={<ProfileChooser onItemClicked={onAccountItemClick} />}
       />
     </View>
   );
