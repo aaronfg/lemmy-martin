046dc3a42bc3ea126d10eb65be93687d7dc24f22
aaronfg <afg@aaronfg.com>
2023-06-25 18:51:48 -0600

    unit tests fore dateutils complete

---
+++ __tests__/utils/date-utils-test.ts
@@ -0,0 +1,43 @@
+import { DateTime, Settings } from 'luxon';
+import { DateUtils } from '../../src/utils/DateUtils';
+describe('DateUtils()', () => {
+  describe('getUserFriendlyPostDate()', () => {
+    test('getUserFriendlyPostDate() with date 10 hrs ago returns correctly', () => {
+      const expectedNow = DateTime.local(2023, 6, 1, 10, 0, 0);
+      Settings.now = () => expectedNow.toMillis();
+      const fakeDate = new Date('6/1/2023');
+      const iso = fakeDate.toISOString();
+      expect(DateUtils.getUserFriendlyPostDate(iso)).toMatchInlineSnapshot(
+        `"10 hours ago"`,
+      );
+    });
+
+    test('getUserFriendlyPostDate() with date 10 days ago returns correctly', () => {
+      const expectedNow = DateTime.local(2023, 6, 11, 23, 0, 0);
+      Settings.now = () => expectedNow.toMillis();
+      const fakeDate = new Date('6/1/2023');
+      const iso = fakeDate.toISOString();
+      expect(DateUtils.getUserFriendlyPostDate(iso)).toMatchInlineSnapshot(
+        `"10 days ago"`,
+      );
+    });
+  });
+
+  describe('getSingularOrPluralDuration()', () => {
+    test('getSingularOrPluralDuration() with 0 returns plural phrase', () => {
+      expect(
+        DateUtils.getSingularOrPluralDuration(0, 'hour'),
+      ).toMatchInlineSnapshot(`"0 hours ago"`);
+    });
+    test('getSingularOrPluralDuration() with 1 returns singular phrase', () => {
+      expect(
+        DateUtils.getSingularOrPluralDuration(1, 'hour'),
+      ).toMatchInlineSnapshot(`"1 hour ago"`);
+    });
+    test('getSingularOrPluralDuration() with 4 returns plural phrase', () => {
+      expect(
+        DateUtils.getSingularOrPluralDuration(4, 'hour'),
+      ).toMatchInlineSnapshot(`"4 hours ago"`);
+    });
+  });
+});
--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -3,22 +3,60 @@ export class DateUtils {
   static getUserFriendlyPostDate = (dateString: string): string => {
     console.log('new Date: ' + dateString);
     const date = DateTime.fromISO(dateString, { zone: 'utc' });
-    const diff = DateTime.now().diff(date, ['days', 'hours', 'minutes']);
+    const diff = DateTime.now().diff(date, [
+      'years',
+      'months',
+      'days',
+      'hours',
+      'minutes',
+    ]);
+    const years = diff.years;
+    const months = diff.months;
     const days = diff.days;
     const hours = diff.hours;
     const minutes = Math.floor(diff.minutes);
     console.log('\tdays: ' + days);
     console.log('\thours: ' + hours);
     console.log('\tminutes: ' + minutes + '\n\n');
-    if (days > 1) {
-      return `${days} days ago`;
-    } else if (hours < 1) {
-      if (minutes < 1) {
-        return 'Now';
-      }
-      return `${minutes} minutes ago`;
+    if (years >= 1) {
+      return this.getSingularOrPluralDuration(years, 'year');
+    }
+    if (months >= 1) {
+      return this.getSingularOrPluralDuration(months, 'month');
+    }
+    if (days >= 1) {
+      return this.getSingularOrPluralDuration(days, 'day');
+    }
+    if (hours >= 1) {
+      return this.getSingularOrPluralDuration(hours, 'hour');
+    }
+    if (minutes >= 2) {
+      return this.getSingularOrPluralDuration(minutes, 'minute');
+    }
+    return 'Now';
+    // if (days > 1) {
+    //   return `${days} days ago`;
+    // } else if (hours < 1) {
+    //   if (minutes < 1) {
+    //     return 'Now';
+    //   }
+    //   return `${minutes} minutes ago`;
+    // } else {
+    //   return `${hours} hours ago`;
+    // }
+  };
+
+  static getSingularOrPluralDuration = (
+    dateNum: number,
+    singularString: string,
+  ): string => {
+    if (dateNum < 1) {
+      return `${dateNum} ${singularString}s ago`;
+    }
+    if (dateNum === 1) {
+      return `${dateNum} ${singularString} ago`;
     } else {
-      return `${hours} hours ago`;
+      return `${dateNum} ${singularString}s ago`;
     }
   };
 }
